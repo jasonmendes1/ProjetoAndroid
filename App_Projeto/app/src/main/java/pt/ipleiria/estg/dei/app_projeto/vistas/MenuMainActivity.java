2932bb302ee88ae0effcb07cc0827042e1d900d0
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-12-09 16:46:51 +0000

    MENU + STRINGS + LOGINERRO + IMAGENS

---
+++ App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/vistas/MenuMainActivity.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.app_projeto.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.app_projeto.R;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+    }
+}
