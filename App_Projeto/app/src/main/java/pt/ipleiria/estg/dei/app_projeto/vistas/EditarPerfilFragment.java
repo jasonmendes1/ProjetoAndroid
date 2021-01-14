d21366e9c24cdb075853cc5aa007047794ca8d0b
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-01-14 16:34:21 +0000

    perfil + editarperfil

---
+++ App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/vistas/EditarPerfilFragment.java
@@ -0,0 +1,20 @@
+package pt.ipleiria.estg.dei.app_projeto.vistas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import pt.ipleiria.estg.dei.app_projeto.R;
+
+public class EditarPerfilFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editar_perfil, container, false);
+    }
+}
--- App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/vistas/PerfilFragment.java
+++ App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/vistas/PerfilFragment.java
@@ -1,27 +1,25 @@
 package pt.ipleiria.estg.dei.app_projeto.vistas;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import pt.ipleiria.estg.dei.app_projeto.R;
 
 public class PerfilFragment extends Fragment {
 
-    public PerfilFragment() {
-        // Required empty public constructor
-    }
+    private TextView tvNome, tvSexo2, tvEmail2, tvDataNasc2, tvDataInicio, tvDataExpirar;
+    private SharedPreferences sharedPreferences;
+
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
     }
