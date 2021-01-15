7f09a427ca13d28de64c6ef8f272b68f586e210b
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-01-15 02:05:34 +0000

    a

---
+++ App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/MainActivityPlanosTreino.java
@@ -0,0 +1,14 @@
+package pt.ipleiria.estg.dei.app_projeto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityPlanosTreino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_planos_treino);
+    }
+}
