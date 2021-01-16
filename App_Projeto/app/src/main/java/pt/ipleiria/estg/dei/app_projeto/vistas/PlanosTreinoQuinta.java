1105b8a7f7ddb287e350ee81e4db9e2b549df89b
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-01-16 02:52:23 +0000

    Planos Treino Dias todos

---
+++ App_Projeto/app/src/main/java/pt/ipleiria/estg/dei/app_projeto/vistas/PlanosTreinoQuinta.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.app_projeto.vistas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pt.ipleiria.estg.dei.app_projeto.R;
+
+public class PlanosTreinoQuinta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planos_treino_quinta);
+    }
+}
